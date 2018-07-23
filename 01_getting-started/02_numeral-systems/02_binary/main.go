79c47afd7da2e661ecc03fba80221c1ebf2e1865
lindsaycriswell <lindsaycriswell@gmail.com>
2018-07-23 17:07:51 -0400

    numeral systems

---
+++ 01_getting-started/02_numeral-systems/02_binary/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d - %b \n", 42, 42)
+}
+
+// Printf = "print with formatting"
+// takes 'variadic' number of arguments - needs to correspond to number of 'format verbs':
+// %d = decimal
+// %b = binary
+
+// Prints '42 - 101010'
---
+++ 01_getting-started/02_numeral-systems/03_hexidecimal/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d - %b - %x \n", 42, 42, 42)
+}
+
+// %x = hex
+// Prints '42 - 101010 - 2a'
