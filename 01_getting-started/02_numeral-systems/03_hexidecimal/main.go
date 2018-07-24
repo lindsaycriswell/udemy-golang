91a931183f8b5263f18831092e5f10d470ced79d
lindsaycriswell <lindsaycriswell@gmail.com>
2018-07-24 10:27:37 -0400

    numerals with loops

--- 01_getting-started/02_numeral-systems/03_hexidecimal/main.go
+++ 01_getting-started/02_numeral-systems/03_hexidecimal/main.go
@@ -4,7 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Printf("%d - %b - %x \n", 42, 42, 42)
+	fmt.Printf("%d - %b - %#x \n", 42, 42, 42)
+	fmt.Printf("%d - %b - %#X \n", 42, 42, 42)
+	fmt.Printf("%d \t %b \t %#X \n", 42, 42, 42)
 }
 
 // %x = hex
-// Prints '42 - 101010 - 2a'
+
+// Prints:
+// 42 - 101010 - 2a
+// 42 - 101010 - 0x2a
+// 42 - 101010 - 0X2A
+// 42 	 101010 	 0X2A
