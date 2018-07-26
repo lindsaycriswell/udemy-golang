7cd36313082983a97e0e10d91170fdf97a2dfd92
lindsaycriswell <lindsaycriswell@gmail.com>
2018-07-26 17:21:22 -0400

    more runes

--- 10_for_loop/07_rune_loop_UTF8/01/main.go
+++ 10_for_loop/07_rune_loop_UTF8/01/main.go
@@ -6,4 +6,9 @@ func main() {
 	for i := 0; i < 150; i++ {
 		fmt.Println(i, "-", string(i), "-", []byte(string(i)))
 	}
+	foo := 'a'
+	// single quotes make characters into runes!
+	fmt.Println(foo)
+	fmt.Printf("%T \n", foo)
+	fmt.Printf("%T \n", rune(foo))
 }
